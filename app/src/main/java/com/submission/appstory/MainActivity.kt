795b65f4423c58a1a61f65983d27fa7f71a5d4dc
lckmnzans <lckmnzans@gmail.com>
2023-05-25 14:03:27 +0700

    create Menu

--- app/src/main/java/com/submission/appstory/MainActivity.kt
+++ app/src/main/java/com/submission/appstory/MainActivity.kt
@@ -2,10 +2,20 @@ package com.submission.appstory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.submission.appstory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_action, menu)
+        return true
     }
 }
